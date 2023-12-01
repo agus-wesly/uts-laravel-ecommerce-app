4b95d22579d6220ef2c82933d4b630ed2fe4707a
aguswesly <agustchannel@gmail.com>
2023-12-01 20:17:21 +0700

    feat : add INFORMASIPROYEK.txt

--- routes/web.php
+++ routes/web.php
@@ -14,14 +14,9 @@ use Inertia\Inertia;
 
 
 /*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
+NIM     : 10121231
+Nama    : Agustinus Wesly Sitanggang
+Kelas   : IF-6
 */
 
 Route::middleware(['auth'])->group(function () {
